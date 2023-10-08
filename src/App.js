3615e992c0ba0653198da67f28d7c01b7cb05820
Junaid <junaidhayat33303@gmail.com>
2023-10-08 22:58:26 +0500

    redux commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import store from './store';
 
 function App() {
+
+  store.subscribe(() => console.log(store.getState()))
+
+
   return (
     <div>
-      <h1>Hello Jarvizs</h1>
+      <h1>Hello Jarviz</h1>
+      <h4>React Redux</h4>
+      <div>
+        <button>-</button>
+        <input name='quantity' type="text" />
+        <button>+</button>
+      </div>
     </div>
   );
 }
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import reducer from './upDown'
+
+const rootReducer = combineReducers({
+    reducer,
+})
+
+export default rootReducer;
